899abbb7a6a6f9f96887444fb3737789d1601625
Tom Walk <tcwalk@gmail.com>
2017-04-07 17:03:47 -0500

    Update user root

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
    if Rails.env.production?
-      root 'application#hello'
+      root 'users#index'
+#      root 'application#hello'
    end
 #   if Rails.env.development?
+   #   
 #      root 'application#hello'
 #   end
 
